58fe1a98fc021afa86c304aa58504930d7862cbf
palmolive <matteo.marucci@telsy.it>
2023-09-01 09:04:46 +0200

    update

--- transaction_extractor/pkg/database/address_status/address_status.go
+++ transaction_extractor/pkg/database/address_status/address_status.go
@@ -7,11 +7,10 @@ func (AddressStatus) TableName() string {
 }
 
 type AddressStatus struct {
-	AddressStatusId      int    `gorm:"column:AddressStatusId;type:int;primaryKey;autoIncrement"`
-	AddressId            string `gorm:"column:AddressId;varchar(1024)"`
+	AddressId            string `gorm:"column:AddressId;primaryKey;varchar(1024)"`
 	Address              address.Address
 	TokenName            string  `gorm:"column:TokenName;varchar(1024);not null"`
 	TokenSymbol          string  `gorm:"column:TokenSymbol;varchar(256);not null"`
-	TokenContractAddress string  `gorm:"column:TokenContractAddress;varchar(1024);not null"`
+	TokenContractAddress string  `gorm:"column:TokenContractAddress;primaryKey;varchar(1024);not null"`
 	TokenAmount          float64 `gorm:"column:TokenAmount;not null"`
 }
