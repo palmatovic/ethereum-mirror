8c1ca09cd0cef8e3c2e531570fa66d70de42f371
Davide <davide.salvadore@telsy.it>
2023-09-05 22:04:52 +0200

    Scam check function added

--- transaction_extractor/pkg/cron/sync/sync.go
+++ transaction_extractor/pkg/cron/sync/sync.go
@@ -47,7 +47,13 @@ func (e *Env) SyncTransactions() (response interface{}, err error) {
 				//GET TOKEN TRANSFER FOR ADDRESS
 
 				for _, sas := range savedAddressStatuses {
-					address_transfers_service.GetAddressTokenTransfers(e.Database, address, sas, e.Browser)
+					//address_transfers_service.GetAddressTokenTransfers(e.Database, address, sas, e.Browser)
+					check, err := address_transfers_service.ScamCheck(sas.TokenContractAddress, e.Browser)
+					if err != nil {
+						return nil, err
+					}
+					println(sas.TokenContractAddress)
+					println(check)
 				}
 			}
 
--- transaction_extractor/pkg/service/address_transfers/address_transfers.go
+++ transaction_extractor/pkg/service/address_transfers/address_transfers.go
@@ -26,7 +26,7 @@ func GetAddressTokenTransfers(db *gorm.DB, address string, address_status addres
 	}
 	page.WaitForTimeout(2000)
 
-	page.Locator("xpath=//html/body/div[1]/div[2]/div/div[2]/div[1]/div[3]/div/div/div[2]/div[1]/div/div")
+	//page.Locator("xpath=//html/body/div[1]/div[2]/div/div[2]/div[1]/div[3]/div/div/div[2]/div[1]/div/div")
 
 	expandTable := page.Locator("xpath=//html/body/div[1]/div[2]/div/div[2]/div[1]/div[2]/div[5]")
 	err = expandTable.Click()
@@ -133,5 +133,46 @@ func GetAddressTokenTransfers(db *gorm.DB, address string, address_status addres
 			}
 
 		}
+	}
+
+}
+
+func ScamCheck(tokenAddress string, browser playwright.Browser) (bool, error) {
+	page, err := browser.NewPage()
+	if err != nil {
+		return false, err
+	}
+
+	_, err = page.Goto("https://gopluslabs.io/token-security/1/" + tokenAddress)
+	if err != nil {
+		return false, err
+	}
+
+	err = page.WaitForLoadState()
+	if err != nil {
+		return false, err
+	}
+
+	riskyItems := page.Locator("xpath=//html/body/div[1]/div[2]/div[2]/div[1]/div/div[3]/div[1]/div/div[2]")
+	riskyItemsNum, err := riskyItems.TextContent()
+	if err != nil {
+		return false, err
+	}
+
+	attentionItems := page.Locator("xpath=//html/body/div[1]/div[2]/div[2]/div[1]/div/div[3]/div[1]/div/div[2]")
+	attentionItemsNum, err := attentionItems.TextContent()
+	if err != nil {
+		return false, err
+	}
+	println(attentionItemsNum)
+
+	err := page.Close()
+	if err != nil {
+		return false, err
+	}
+	if riskyItemsNum != "0" {
+		return true, nil
+	} else {
+		return false, err
 	}
 }
