478e9f234fc33a480f423d801685fa40d18af21d
palmolive <matteo.marucci@telsy.it>
2023-10-11 02:03:33 +0200

    update

--- wallet_synchronizer/pkg/service/token/graphql/graphql.go
+++ wallet_synchronizer/pkg/service/token/graphql/graphql.go
@@ -22,14 +22,6 @@ func NewService(
 	}
 }
 
-// TokenId        string    `json:"token_id" gorm:"column:TokenId;primaryKey"`
-// Name           string    `json:"name" gorm:"column:Name;not null"`
-// Symbol         string    `json:"symbol" gorm:"column:Symbol;not null"`
-// Decimals       int       `json:"decimals" gorm:"column:Decimals;not null"`
-// CreatedAt      time.Time `json:"created_at" gorm:"column:CreatedAt;autoCreateTime"`
-// Logo           string    `json:"logo" gorm:"column:Logo"`
-// GoPlusResponse []byte    `json:"go_plus_response" gorm:"column:GoPlusResponse"`
-
 var tokenType = graphql.NewObject(graphql.ObjectConfig{
 	Name: "token",
 	Fields: graphql.Fields{
@@ -67,7 +59,7 @@ var tokenType = graphql.NewObject(graphql.ObjectConfig{
 func (s *Service) Schema() graphql.Schema {
 
 	var rootQuery = graphql.NewObject(graphql.ObjectConfig{
-		Name: "RootQuery",
+		Name: "TokenQuery",
 		Fields: graphql.Fields{
 			"token": &graphql.Field{
 				Type: tokenType,
