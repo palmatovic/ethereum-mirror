8ba9fa00c95ceb9bd11f904085c7541b5b71839a
palmolive <matteo.marucci@telsy.it>
2023-10-16 00:29:30 +0200

    Update

--- auth/pkg/database/user/user.go
+++ auth/pkg/database/user/user.go
@@ -3,24 +3,14 @@ package user
 import "time"
 
 type User struct {
-	UserId               int       `json:"-" gorm:"column:UserId;primaryKey;autoIncrement"`
-	Username             string    `json:"-" gorm:"column:Username;not null;uniqueKey:UsernameIdx"`
-	UserEnabled          bool      `json:"-" gorm:"column:UserEnabled;default:0"`
-	Email                string    `json:"-" gorm:"column:Email;not null;uniqueKey:EmailIdx"`
-	Password             string    `json:"-" gorm:"column:Password;not null"`
-	PasswordEnabled      bool      `json:"-" gorm:"column:PasswordEnabled;default:0"`
-	PasswordExpirationAt time.Time `json:"-" gorm:"column:PasswordExpirationAt"`
-	PasswordExpired      bool      `json:"-" gorm:"column:PasswordExpired;default:0"`
-	PasswordAttempts     int       `json:"-" gorm:"column:PasswordAttempts"`
-	MasterPasswordKey    string    `json:"-" gorm:"column:MasterKey"` // MasterPasswordKey used for reset lost/forgotten password
-	Name                 string    `json:"-" gorm:"column:Name;not null"`
-	Surname              string    `json:"-" gorm:"column:Surname;not null"`
-	Dob                  time.Time `json:"-" gorm:"column:Dob;type:date;not null"`
-	CreatedAt            time.Time `json:"-" gorm:"column:CreatedAt;autoCreateTime"`
-	UpdatedAt            time.Time `json:"-" gorm:"column:UpdatedAt;autoUpdateTime"`
-	TwoFAKey             string    `json:"-" gorm:"column:2FAKey"`
-	TwoFAEnabled         bool      `json:"-" gorm:"column:2FAEnabled;default:0"`
-	MasterTwoFAKey       string    `json:"-" gorm:"column:MasterTwoFAKey"` // MasterTwoFAKey used for reset lost 2FA
+	UserId    int       `json:"-" gorm:"column:UserId;primaryKey;autoIncrement"`
+	Username  string    `json:"-" gorm:"column:Username;not null;uniqueKey:UsernameIdx"`
+	Email     string    `json:"-" gorm:"column:Email;not null;uniqueKey:EmailIdx"`
+	Name      string    `json:"-" gorm:"column:Name;not null"`
+	Surname   string    `json:"-" gorm:"column:Surname;not null"`
+	Dob       time.Time `json:"-" gorm:"column:Dob;type:date;not null"`
+	CreatedAt time.Time `json:"-" gorm:"column:CreatedAt;autoCreateTime"`
+	UpdatedAt time.Time `json:"-" gorm:"column:UpdatedAt;autoUpdateTime"`
 }
 
 func (User) TableName() string {
--- auth/pkg/database/user_product/user_product.go
+++ auth/pkg/database/user_product/user_product.go
@@ -12,8 +12,18 @@ type UserProduct struct {
 	user.User
 	ProductId string `json:"product_id" gorm:"ProductId;uniqueIndex:UserProductIdx"`
 	product.Product
-	CreatedAt time.Time `json:"created_at" gorm:"column:CreatedAt;autoCreateTime"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"column:UpdatedAt;autoUpdateTime"`
+	UserEnabled          bool      `json:"-" gorm:"column:UserEnabled;default:0"`
+	Password             string    `json:"-" gorm:"column:Password;not null"`
+	PasswordEnabled      bool      `json:"-" gorm:"column:PasswordEnabled;default:0"`
+	PasswordExpirationAt time.Time `json:"-" gorm:"column:PasswordExpirationAt"`
+	PasswordExpired      bool      `json:"-" gorm:"column:PasswordExpired;default:0"`
+	PasswordAttempts     int       `json:"-" gorm:"column:PasswordAttempts"`
+	MasterPasswordKey    string    `json:"-" gorm:"column:MasterKey"` // MasterPasswordKey used for reset lost/forgotten password
+	TwoFAKey             string    `json:"-" gorm:"column:2FAKey"`
+	TwoFAEnabled         bool      `json:"-" gorm:"column:2FAEnabled;default:0"`
+	MasterTwoFAKey       string    `json:"-" gorm:"column:MasterTwoFAKey"` // MasterTwoFAKey used for reset lost 2FA
+	CreatedAt            time.Time `json:"created_at" gorm:"column:CreatedAt;autoCreateTime"`
+	UpdatedAt            time.Time `json:"updated_at" gorm:"column:UpdatedAt;autoUpdateTime"`
 }
 
 func (UserProduct) TableName() string {
