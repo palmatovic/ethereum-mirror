debad377cb9cc357c9330a70e94afd9e54e077b7
palmolive <matteo.marucci@telsy.it>
2023-10-16 01:34:47 +0200

    Update product.go

--- auth/pkg/database/product/product.go
+++ auth/pkg/database/product/product.go
@@ -3,9 +3,12 @@ package product
 import "time"
 
 type Product struct {
-	ProductId string    `json:"product_id" gorm:"column:ProductId;primaryKey"`
-	CreatedAt time.Time `json:"created_at" gorm:"column:CreatedAt;autoCreateTime"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"column:UpdatedAt;autoUpdateTime"`
+	ProductId                   string    `json:"product_id" gorm:"column:ProductId;primaryKey"`
+	Rsa256PK                    string    `json:"-" gorm:"column:Rsa256PK"`
+	AccessTokenDurationMinutes  int       `json:"access_token_duration_minutes" gorm:"column:AccessTokenDurationMinutes"`
+	RefreshTokenDurationMinutes int       `json:"refresh_token_duration_minutes" gorm:"column:RefreshTokenDurationMinutes"`
+	CreatedAt                   time.Time `json:"created_at" gorm:"column:CreatedAt;autoCreateTime"`
+	UpdatedAt                   time.Time `json:"updated_at" gorm:"column:UpdatedAt;autoUpdateTime"`
 }
 
 func (Product) TableName() string {
